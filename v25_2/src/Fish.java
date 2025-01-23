123edf1b98109a198a176748d7a74090b486b083
yalerikk <130097833+yalerikk@users.noreply.github.com>
2025-01-23 22:39:15 +0300

    Добавила 2 лабу
    Также немного усовершенствовала код относительно 1 лабы, больше проверок

---
+++ v25_2/src/Fish.java
@@ -0,0 +1,45 @@
+public abstract class Fish implements Comparable<Fish> {
+    private String name;
+    private int lengthOfLife;
+    private int size;
+    protected double cost;
+
+    public Fish(String name, int lengthOfLife, int size) {
+        setName(name);
+        setLengthOfLife(lengthOfLife);
+        setSize(size);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLengthOfLife() {
+        return lengthOfLife;
+    }
+
+    public void setLengthOfLife(int lengthOfLife) {
+        this.lengthOfLife = lengthOfLife;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public int compareTo(Fish o) {
+        return this.getLengthOfLife() - o.getLengthOfLife();
+    }
+}
